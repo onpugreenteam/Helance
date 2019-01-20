70506c6c1c49105ff37c0e20983942132ee0d21d
User <haker658@gmail.com>
2019-01-20 16:21:53 +0200

    Обновлено окно поиска собеседника

--- Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/WaitingTimeActivity.java
+++ Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/WaitingTimeActivity.java
@@ -2,11 +2,16 @@ package com.ranpeak.ProjectX.activity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.ranpeak.ProjectX.R;
 
 public class WaitingTimeActivity extends AppCompatActivity {
+
+    private int seconds;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,33 @@ public class WaitingTimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_waiting_time);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        runtimer(true);
     }
+
+
+
+    private void runtimer(final Boolean running){
+
+        final TextView textView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                int second = seconds%60;
+                int minutes = (seconds%3600)/60;
+
+                String time = String.format("%02d:%02d", minutes, second);
+                textView.setText(time);
+
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+    }
+
 }
