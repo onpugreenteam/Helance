ccf1fd08880b16aec8442e172d50a3660718d8f5
Projectxai174@gmail.com <HerokuProjectxAI-174>
2019-02-16 23:16:19 +0200

    made a registration activities (probably not final result)

---
+++ Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/registration/RegistrationActivity1.java
@@ -0,0 +1,155 @@
+package com.ranpeak.ProjectX.activity.registration;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.ranpeak.ProjectX.R;
+import com.ranpeak.ProjectX.constant.Constants;
+import com.ranpeak.ProjectX.user.data.RequestHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+public class RegistrationActivity1 extends AppCompatActivity{
+
+    private final static int REGISTRATION_ACTIVITY1 = R.layout.activity_registration1;
+    private Button nextButton;
+    private TextInputLayout register_email;
+    private boolean email_exists = false;
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            nextButton.setEnabled(!register_email.getEditText().getText().toString().trim().isEmpty()
+                    && isEmailValid(register_email.getEditText().getText().toString())
+                    && !emailExists()
+            );
+            if(nextButton.isEnabled()){
+                register_email.setError(null);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(REGISTRATION_ACTIVITY1);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        register_email = findViewById(R.id.registration_email);
+        nextButton = findViewById(R.id.registration_button_1);
+        nextButton.setEnabled(false);
+        register_email.getEditText().addTextChangedListener(textWatcher);
+    }
+    public void clickRegistration_1(View view){
+            Intent intent = new Intent(getApplicationContext(), RegistrationActivity2.class);
+            intent.putExtra("email",register_email.getEditText().getText().toString().trim());
+            startActivity(intent);
+
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    private boolean emailExists(){
+        checkEmail();
+        if (email_exists) {
+//            register_email.setError(getString(R.string.error_exist_email));
+            return true;
+        } else {
+//            register_email.setError(null);
+            return false;
+        }
+    }
+    private void checkEmail() {
+        final String email = register_email.getEditText().getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.URL.CHECK_EMAIL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if(jsonObject.getString("login").equals("no")){
+                                Toast.makeText(getApplicationContext(),"This email already registered",Toast.LENGTH_LONG);
+                                register_email.setError(getString(R.string.error_exist_email));
+                                email_exists = true;
+
+                            }else if(jsonObject.getString("login").equals("ok")){
+                                register_email.setError(null);
+                                email_exists = false;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Please turn on Internet", Toast.LENGTH_LONG).show();
+                    }
+                }){
+
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String,String> params = new HashMap<>();
+                params.put("email",email);
+                return params;
+            }
+        };
+
+        RequestHandler.getmInstance(this).addToRequestQueue(stringRequest);
+    }
+    private boolean isEmailValid(String email){
+//        boolean isValid = false;
+//        try {
+//            //
+//            // Create InternetAddress object and validated the supplied
+//            // address which is this case is an email address.
+//            InternetAddress internetAddress = new InternetAddress(email);
+//            internetAddress.validate();
+//            isValid = true;
+//        } catch (AddressException e) {
+//            e.printStackTrace();
+//        }
+//        return isValid;
+                String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+
+    }
+}
---
+++ Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/registration/RegistrationActivity2.java
@@ -0,0 +1,160 @@
+package com.ranpeak.ProjectX.activity.registration;
+
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.ranpeak.ProjectX.R;
+import com.ranpeak.ProjectX.constant.Constants;
+
+public class RegistrationActivity2 extends AppCompatActivity/* implements CountryListActivity.OnInputListener*/ {
+
+    private final static int REGISTRATION_ACTIVITY2 = R.layout.activity_registration2;
+
+    private EditText register_name;
+    private EditText register_age;
+    private RadioGroup register_gender;
+    private RadioButton register_gender_male;
+    private RadioButton register_gender_female;
+    private RadioButton radioButton;
+    private String gender;
+
+    private TextView register_country;
+    private static String email;
+
+    public void setCountry(String country) {
+        this.register_country.setText(country);
+        this.register_country.setTextColor(Color.parseColor("#D81B60"));
+    }
+
+    //    private AutoCompleteTextView register_country;
+    private Button nextButton;
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            nextButton.setEnabled(!register_name.getText().toString().trim().isEmpty()
+                    && !register_age.getText().toString().trim().isEmpty()
+//                    && !register_country.getText().toString().trim().isEmpty()
+                    && stringContainsItemFromList(register_country.getText().toString(),
+                    Constants.Values.COUNTRIES)
+                    && checkGender()
+//                    && !register_gender.getText().toString().trim().isEmpty()
+            );
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(REGISTRATION_ACTIVITY2);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        email = getIntent().getStringExtra("email");
+
+        // Находим и передаем локальным переменным обьекты activity_registration
+        register_name = findViewById(R.id.register_name);
+        register_name.addTextChangedListener(textWatcher);
+        register_age = findViewById(R.id.register_age);
+        register_age.addTextChangedListener(textWatcher);
+        register_gender = findViewById(R.id.register_gender_group);
+        register_gender_male = findViewById(R.id.register_gender_male);
+        register_gender_female = findViewById(R.id.register_gender_female);
+//        register_gender.get.addTextChangedListener(textWatcher);
+
+
+        // start fragmentActivity to choose country
+        final FragmentManager fm = getFragmentManager();
+        final CountryListActivity  countryListActivity = new CountryListActivity();
+        register_country = findViewById(R.id.register_country);
+        register_country.addTextChangedListener(textWatcher);
+        register_country.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countryListActivity.show(fm, "Country lists");
+            }
+        });
+
+        nextButton = findViewById(R.id.registration_button_2);
+        nextButton.setEnabled(false);
+        nextButton.addTextChangedListener(textWatcher);
+    }
+
+    public void clickRegistration_2(View view){
+        Intent intent = new Intent(getApplicationContext(), RegistrationActivity3.class);
+        intent.putExtra("email", email);
+        intent.putExtra("name", register_name.getText().toString().trim());
+        intent.putExtra("age", register_age.getText().toString().trim());
+//        intent.putExtra("gender", register_gender.getText().toString().trim());
+        intent.putExtra("gender", gender);
+        intent.putExtra("country", register_country.getText().toString().trim());
+        startActivity(intent);
+
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+    }
+    private static boolean stringContainsItemFromList(String inputStr, String[] items)
+    {
+        for(int i =0; i < items.length; i++)
+        {
+            if(inputStr.contains(items[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void checkRadioButton(View v){
+        int radioId = register_gender.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+        gender = radioButton.getText().toString();
+    }
+
+    private boolean checkGender(){
+        if(register_gender_male.isChecked()){
+            gender = register_gender_male.getText().toString();
+            return true;
+        } else if(register_gender_female.isChecked()){
+            gender = register_gender_female.getText().toString();
+            return true;
+        }
+        return false;
+    }
+
+//    @Override
+//    public void sendInput(String country) {
+////        setCountry(country);
+//        this.register_country.setText(country);
+//        this.register_country.setTextColor(Color.parseColor("#D81B60"));
+//    }
+}
