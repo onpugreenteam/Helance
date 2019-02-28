62a6d3aaaec32e1db091716a05268c15c6dbcb82
Projectxai174@gmail.com <HerokuProjectxAI-174>
2019-02-28 20:14:53 +0200

    create CreatingTask activity

--- Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/LobbyActivity.java
+++ Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/LobbyActivity.java
@@ -30,8 +30,7 @@ public class LobbyActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
+                startActivity(new Intent(getApplicationContext(), CreatingTaskActivity.class));
             }
         });
 
