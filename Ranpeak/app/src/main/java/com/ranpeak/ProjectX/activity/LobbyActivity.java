75f3f838e41c2b8e8cbf125f29d66b891ad86678
User <haker658@gmail.com>
2019-02-28 19:49:20 +0200

    Add + button

--- Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/LobbyActivity.java
+++ Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/LobbyActivity.java
@@ -3,14 +3,20 @@ package com.ranpeak.ProjectX.activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
 
 import com.ranpeak.ProjectX.R;
 
 public class LobbyActivity extends AppCompatActivity {
 
    private final static int LOBBY_ACTIVITY = R.layout.activity_lobby;
+
+   private FloatingActionButton fab;
+   private ListView listView;
+
 
 
     @Override
@@ -19,10 +25,22 @@ public class LobbyActivity extends AppCompatActivity {
         setContentView(LOBBY_ACTIVITY);
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-    }
 
+        fab.findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
+    }
 
     public void ClickProfile(View view){
         startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
     }
+
+
 }
--- Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/registration/RegistrationActivity2.java
+++ Ranpeak/app/src/main/java/com/ranpeak/ProjectX/activity/registration/RegistrationActivity2.java
@@ -1,8 +1,6 @@
 package com.ranpeak.ProjectX.activity.registration;
 
-import android.app.AlertDialog;
 import android.app.FragmentManager;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
@@ -13,10 +11,8 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.NumberPicker;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
+
 import com.ranpeak.ProjectX.R;
 import com.ranpeak.ProjectX.constant.Constants;
 
