88bc2cf6a6b2b3041ad53e79f341d0cdfdcf8254
JekaSambo <dobrovolskij901@gmail.com>
2019-04-17 11:10:28 +0300

    Merge branch 'master' of C:\Users\JekaSambo\greenteamproject with conflicts.

---
+++ project/app/src/main/java/com/ranpeak/ProjectX/activity/creatingResume/ViewResumeActivity.java
@@ -0,0 +1,15 @@
+package com.ranpeak.ProjectX.activity.creatingResume;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ranpeak.ProjectX.R;
+
+public class ViewResumeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_resume);
+    }
+}
