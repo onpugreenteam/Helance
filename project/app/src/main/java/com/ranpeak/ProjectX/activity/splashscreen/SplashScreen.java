1463b38a2e47b9cab0f456f366ad0f4f15f79163
User <haker658@gmail.com>
2019-05-11 23:05:21 +0300

    Refactor lobby

--- project/app/src/main/java/com/ranpeak/ProjectX/activity/splashscreen/SplashScreen.java
+++ project/app/src/main/java/com/ranpeak/ProjectX/activity/splashscreen/SplashScreen.java
@@ -1,4 +1,4 @@
-package com.ranpeak.ProjectX.splashscreen;
+package com.ranpeak.ProjectX.activity.splashscreen;
 
 import android.content.Context;
 import android.content.Intent;
@@ -15,26 +15,29 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.toolbox.StringRequest;
 import com.ranpeak.ProjectX.R;
 import com.ranpeak.ProjectX.activity.lobby.forGuestUsers.LobbyForGuestActivity;
-import com.ranpeak.ProjectX.networking.volley.Constants;
-import com.ranpeak.ProjectX.networking.volley.RequestHandler;
+import com.ranpeak.ProjectX.activity.splashscreen.commands.SplashNavigator;
+import com.ranpeak.ProjectX.activity.splashscreen.viewModel.SplashViewModel;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity implements SplashNavigator {
 
     private final static int SPLASH_ACTIVITY = R.layout.activity_splash_screen;
     private TextView textView;
     private ImageView imageView;
     private TextView connectionStatus;
-
+    private SplashViewModel splashViewModel;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(SPLASH_ACTIVITY);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        splashViewModel = new SplashViewModel(getApplicationContext());
+        splashViewModel.setNavigator(this);
+
         checkInternetConnection();
         findViewById();
         animate();
@@ -75,7 +78,7 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animation animation) {
                 connectionStatus.setVisibility(View.VISIBLE);
-                getAllLogins();
+                splashViewModel.getAllTask();
             }
 
             @Override
@@ -88,28 +91,6 @@ public class SplashScreen extends AppCompatActivity {
 
     private void startLoadData() {
         finish();
-    }
-
-
-    private void noInternetConnection() {
-        connectionStatus.setText(getString(R.string.internet_check));
-        connectionStatus.setTextColor(getResources().getColor(R.color.colorAccent));
-    }
-
-
-    private void getAllLogins() {
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, Constants.URL.GET_ALL_TASK,
-                response -> {
-                    Toast.makeText(getApplicationContext(), "Data downloading", Toast.LENGTH_SHORT).show();
-                    startLoadData();
-                },
-                error -> {
-                    Toast.makeText(getApplicationContext(), "Server don`t started", Toast.LENGTH_SHORT).show();
-//                    noInternetConnection();
-                    startLoadData();
-                });
-        RequestHandler.getmInstance(this).addToRequestQueue(stringRequest);
     }
 
     private boolean checkInternetConnection() {
@@ -137,5 +118,15 @@ public class SplashScreen extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public void handleError(Throwable throwable) {
+        Toast.makeText(getApplicationContext(), "Server don`t started", Toast.LENGTH_SHORT).show();
+        startLoadData();
+    }
 
+    @Override
+    public void completeLoad() {
+        Toast.makeText(getApplicationContext(), "Data downloading", Toast.LENGTH_SHORT).show();
+        startLoadData();
+    }
 }
